d85337d708e9724fef74baf612764e6816037b02
tongjinlv <179990830@qq.com>
2018-04-22 21:50:01 +0800

    串口驱动

--- apps/gide/serial.js
+++ apps/gide/serial.js
@@ -54,27 +54,25 @@ Serial.downLoad1 = function() {
 }); 
 };
 Serial.downLoad = function() {
-  var arduinoTextarea = document.getElementById('content_arduino');
-  arduinoTextarea.value = Blockly.Arduino.workspaceToCode(Blockly.mainWorkspace); 
   var onReceiveCallback = function(info) {//串口数据接收函数  
 	alert("Revice");  
   };  
   var onConnect = function(connectionInfo) { 
-  chrome.serial.onReceive.addListener(onReceiveCallback);
-  var buffer = new ArrayBuffer(1); 
+  //chrome.serial.onReceive.addListener(onReceiveCallback);
+  var buffer = new ArrayBuffer(100); 
   var connectionId = connectionInfo.connectionId;
   chrome.serial.send(connectionId, buffer, console.log.bind(console));
-  alert(connectionInfo.connectionId);
+//  alert(connectionInfo.connectionId);
   };
-  chrome.serial.connect("COM6", {bitrate: 9600}, onConnect); 
-  var fs = require("fs");  
-  fs.writeFile("main.c", arduinoTextarea.value, function(err) {});
-  var com=document.getElementById("SerailMenu").value;
-  var cp = require('child_process'); //子进程  
-  cp.exec("IAP.exe -w "+com+"  main.bin", function(error, stdout, stderr) {  
-  //alert(error);
-  alert(stdout);
-  console.log(stdout);
-  //alert(stderr);
-}); 
+  var com = document.getElementById("SerailMenu").value; 
+  chrome.serial.connect(com, {bitrate: 9600}, onConnect); 
+  var onDisconnect = function(result) {
+    if (result) {
+      alert("已经断开串行端口连接");
+    } else {
+      alert("断开连接失败");
+    }
+  }
+  alert("dd");
+  chrome.serial.disconnect(connectionId,onDisconnect);
 };
